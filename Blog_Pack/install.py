647728b1206c7c28f401af16f7a4ea21e4ec4da9
Datez <56632970+EzXz@users.noreply.github.com>
2020-02-22 19:43:04 +0700

    https://github.com/CapthaCode404/Blog_Pack

---
+++ Blog_Pack/install.py
@@ -0,0 +1,7 @@
+import os, sys
+os.system('pkg install tor')
+os.system('pip2 install torrequest')
+os.system('pip2 install urllib')
+os.system('pip2 install bs4')
+os.system('pip2 install requests')
+os.system('python2 blog_pack.py')

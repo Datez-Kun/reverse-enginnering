af56a563edd9769280e6b2963dae943562c8ef6e
Datez <56632970+Datez-Kun@users.noreply.github.com>
2020-06-16 01:22:55 +0700

    https://github.com/JustAHackers/perusak

---
+++ perusak/key_file.py
@@ -0,0 +1,7 @@
+# Time Succses Parser : Tue Jun 16 00:39:52 2020
+# Auto Parser Dis Version : 1.1.0
+# Source : https://www.github.com/Datez-Kun
+import hashlib
+#return hashlib.sha512(__file__).hexdigest()
+print hashlib.sha512('perusak.py').hexdigest()
+#'7f84926b65ef83a2d8d13219e5d8ee8ccf1557f0558159945a26561efd8d044fd2bf46e5df351074af71e2eb9c0da569925683fd3e54f31bed836529d7e7ba1a' # result

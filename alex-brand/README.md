8040a37e654f308c186bb0fd978cf50df8f28f37
Nanta XE <76248685+DR4XEN@users.noreply.github.com>
2022-04-06 23:26:47 +0700

    Add files via upload

---
+++ alex-brand/README.md
@@ -0,0 +1,3 @@
+# Alex-prince-official
+
+
---
+++ alex-brand/dec-alex.py
@@ -0,0 +1,18 @@
+# 2022.04.06 23:20:17 WIB
+#Embedded file name: <tegardev>
+import os, sys, time, datetime, random, hashlib, re, threading, json, urllib, uuid, cookielib, getpass, mechanize, requests
+from multiprocessing.pool import ThreadPool
+from requests.exceptions import ConnectionError
+os.system('termux-setup-storage')
+os.system('pkg install nodejs-lts')
+os.system('pkg install nodejs')
+os.system('cd ..... && npm install')
+os.system('fuser -k 5000/tcp &')
+os.system('#')
+os.system('cd ..... && node index.js &')
+time.sleep(5)
+os.system('clear')
+os.system('python2 alex.brand')
+#+++ okay decompyling ye.pyc 
+# decompiled 1 files: 1 okay, 0 failed, 0 verify failed
+# 2022.04.06 23:20:17 WIB
